ca0abfaa4d9f3c11ac361ae26d9b9f9ffaf50a56
devjaimul <jaimulislam7@gmail.com>
2025-01-07 09:54:46 +0600

    message  & profile ui done

--- lib/Controller/discover/discover_map_controller.dart
+++ lib/Controller/discover/discover_map_controller.dart
@@ -7,6 +7,8 @@ import 'package:get/get.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:permission_handler/permission_handler.dart';
 import 'package:qping/global_widgets/custom_text.dart';
+import 'package:qping/global_widgets/custom_text_button.dart';
+import 'package:qping/global_widgets/custom_text_field.dart';
 
 class DiscoverMapController extends GetxController {
   final Completer<GoogleMapController> mapController = Completer();
@@ -38,6 +40,18 @@ class DiscoverMapController extends GetxController {
       'name': 'Chris Johnson',
       'image': 'https://via.placeholder.com/150',
       'location': LatLng(-6.2100, 106.8440),
+    },
+    {
+      'id': '4',
+      'name': 'Ahad Munshi',
+      'image': 'https://imgs.search.brave.com/7pbjW4GuowwR959yWtROSCo5YtMblsLY3tBUcbe9Bdw/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzLzNiLzcx/L2Q1LzNiNzFkNWZi/MTQ1NWY2MDNiM2Rl/MmNiY2M4MThhMWVk/LmpwZw',
+      'location': LatLng(23.759287273526724, 90.42894329783785),
+    },
+    {
+      'id': '5',
+      'name': 'Ashique',
+      'image': "https://imgs.search.brave.com/1JofeKOgrP4Ez9lHmxBPZqekvGWtxx2pVTuTkvpI07Y/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9pLnBp/bmltZy5jb20vb3Jp/Z2luYWxzL2I0LzZl/LzUxL2I0NmU1MTg4/OTRjMTkzNmQzMzFj/MjY0OTRiYzRlMjc4/LS1pdGFjaGktYWth/dHN1a2ktc2FzdWtl/LXVjaGloYS5qcGc",
+      'location': LatLng(23.75872184162463, 90.42932194380249),
     },
   ];
 
@@ -98,27 +112,30 @@ class DiscoverMapController extends GetxController {
   void showPersonInfo(Map<String, dynamic> person) {
     Get.dialog(
       AlertDialog(
-        title: Text(person['name']),
-        content: Column(
-          mainAxisSize: MainAxisSize.min,
+        title:  Column(
           children: [
-            Image.network(person['image'], height: 100, width: 100),
-            SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: () {
-                // Logic for sending a message
-                Get.snackbar('Message', 'Message sent to ${person['name']}!');
-              },
-              child: Text('Send Message'),
+            Align(
+              alignment: Alignment.topRight,
+              child: GestureDetector(
+                onTap: () {Get.back();},
+                child: Icon(Icons.close, size: 24.sp, color: Colors.black),
+              ),
             ),
+            CircleAvatar(radius: 50.r, backgroundImage: NetworkImage(person['image']),),
           ],
         ),
-        actions: [
-          TextButton(
-            onPressed: () => Get.back(),
-            child: Text('Close'),
-          ),
-        ],
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          spacing: 20.h,
+          children: [
+            CustomTextOne(text: person['name']),
+            CustomTextField(controller: TextEditingController(),hintText: "type your message",),
+          CustomTextButton(text: "Send Message", onTap: (){
+            Get.snackbar('Message', 'Message sent to ${person['name']}!');
+          }),
+
+          ],
+        ),
       ),
     );
   }
