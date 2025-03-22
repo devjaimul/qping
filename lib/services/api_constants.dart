9648c4ddc2f033f0e45b5747b2a9b6cf56bc9818
jaimul <jaimulislam7@gmail.com>
2025-03-22 16:18:53 +0600

    password validation changes done

--- lib/services/api_constants.dart
+++ lib/services/api_constants.dart
@@ -1,6 +1,6 @@
 class ApiConstants{
-  static const String baseUrl = "https://qping-ifti.sarv.live";
-  static const String imageBaseUrl = "https://qping-ifti.sarv.live";
+  static const String baseUrl = "https://api.qping.app";
+  static const String imageBaseUrl = "https://api.qping.app";
   // static String googleMapKey="AIzaSyCXGfc8k6U1ma_MQoxOcotVDtrTCqOF0Ok";
 
 }
